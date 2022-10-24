9fd3ebe9bc7c36e74fae9d02edcc9c98cf96c1f5
Mikel-Jon West <dwmyke@gmail.com>
2022-10-24 14:39:53 -0700

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://mikewest.site',
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
